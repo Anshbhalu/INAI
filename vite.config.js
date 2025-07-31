f0eaf186d15fdc856f24133c9a7f7447a0ae5025
Anshbhalu <bhaluansh12@gmail.com>
2025-07-31 16:53:07 +0530

    inai-ansh1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure Vercel knows where to find the output
+  },
+  base: '/', // Keep this '/' unless you're deploying under a subpath
 })
